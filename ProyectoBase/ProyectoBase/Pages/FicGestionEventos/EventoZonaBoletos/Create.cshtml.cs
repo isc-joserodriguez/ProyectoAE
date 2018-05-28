4864d74fcc79038d63604306726b006654a37918
jonathanibarraITT <joguibarrama@ittepic.edu.mx>
2018-05-27 23:31:49 -0600

    cruds jona

---
+++ ProyectoBase/ProyectoBase/Pages/FicGestionEventos/EventoZonaBoletos/Create.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ProyectoBase.Models;
+using ProyectoBase.Models.FicGestionEventos;
+
+namespace ProyectoBase.Pages.FicGestionEventos.EventoZonaBoletos
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ProyectoBase.Models.ApplicationDbContext _context;
+
+        public CreateModel(ProyectoBase.Models.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public res_evento_zona_boletos res_evento_zona_boletos { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.res_evento_zona_boletos.Add(res_evento_zona_boletos);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ ProyectoBase/ProyectoBase/Pages/FicGestionEventos/ZonaServicios/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ProyectoBase.Models;
+using ProyectoBase.Models.FicGestionEventos;
+
+namespace ProyectoBase.Pages.FicGestionEventos.ZonaServicios
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly ProyectoBase.Models.ApplicationDbContext _context;
+
+        public DetailsModel(ProyectoBase.Models.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public res_zonas_servicios res_zonas_servicios { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            res_zonas_servicios = await _context.res_zonas_servicios.SingleOrDefaultAsync(m => m.Id == id);
+
+            if (res_zonas_servicios == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
